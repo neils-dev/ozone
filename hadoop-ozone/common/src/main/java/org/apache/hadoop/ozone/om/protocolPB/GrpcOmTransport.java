df92b14a9a97db417ccfff62a57f4ef8c3b1c0d1
Neil Joshi <neil2021.gres@gmail.com>
2021-07-12 19:18:06 -0600

    Addition for previous Preliminary commit for attempt to pass Minicluster integration tests.  Added GrpcOmTransport support for HA, round robin strategy to connect to leader OM.

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/protocolPB/GrpcOmTransport.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/protocolPB/GrpcOmTransport.java
@@ -18,13 +18,21 @@
 package org.apache.hadoop.ozone.om.protocolPB;
 
 import java.io.IOException;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.lang.reflect.Constructor;
 
+import org.apache.hadoop.hdds.conf.Config;
+import org.apache.hadoop.hdds.conf.ConfigGroup;
+import org.apache.hadoop.hdds.conf.ConfigTag;
 import org.apache.hadoop.hdds.conf.ConfigurationSource;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.retry.RetryProxy;
+import org.apache.hadoop.ozone.OmUtils;
 import org.apache.hadoop.ozone.OzoneConsts;
+import org.apache.hadoop.ozone.ha.ConfUtils;
+import org.apache.hadoop.ozone.om.exceptions.OMNotLeaderException;
 import org.apache.hadoop.ozone.om.ha.OMFailoverProxyProvider;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos.OMRequest;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos.OMResponse;
@@ -32,15 +40,19 @@ import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.security.token.TokenIdentifier;
 import org.apache.hadoop.ozone.security.OzoneTokenIdentifier;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos;
+import org.apache.hadoop.ozone.om.exceptions.OMException;
+import org.apache.hadoop.ozone.om.exceptions.OMException.ResultCodes;
 
-import static org.apache.hadoop.ozone.om.OMConfigKeys.OZONE_OM_ADDRESS_KEY;
+import static org.apache.hadoop.ozone.om.OMConfigKeys.*;
 import static org.apache.hadoop.ozone.protocol.proto
     .OzoneManagerProtocolProtos.OMTokenProto.Type.S3AUTHINFO;
 import static org.apache.hadoop.hdds.HddsUtils.getHostNameFromConfigKeys;
+import static org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos.Status.OK;
 
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.netty.NettyChannelBuilder;
+import io.grpc.Status;
 
 import com.google.common.annotations.VisibleForTesting;
 import org.slf4j.Logger;
@@ -54,32 +66,100 @@ public class GrpcOmTransport implements OmTransport {
       LoggerFactory.getLogger(GrpcOmTransport.class);
 
   private final OMFailoverProxyProvider omFailoverProxyProvider;
+  private static final String CLIENT_NAME = "GrpcOmTransport";
+  private final AtomicBoolean isRunning = new AtomicBoolean(false);
 
   // gRPC specific
-  private final ManagedChannel channel;
+  //private ManagedChannel channel;
 
-  private final OzoneManagerServiceGrpc.OzoneManagerServiceBlockingStub client;
+  private OzoneManagerServiceGrpc.OzoneManagerServiceBlockingStub client;
 
-  //private final String host = "0.0.0.0";
+  private Collection<String> omServiceIds;
+  private List<String> omAddresses;
+  private int hostIndex = 0;
+  private Map<String,
+        OzoneManagerServiceGrpc.OzoneManagerServiceBlockingStub> clients;
+  private Map<String, ManagedChannel> channels;
+  private int lastVisited = -1;
   private String host = "om";
-  private final int port = 8981;
+  private int port = 8981;
+  private ConfigurationSource conf;
+
 
   public GrpcOmTransport(ConfigurationSource conf,
                           UserGroupInformation ugi, String omServiceId)
       throws IOException {
-    Optional<String> omHost = getHostNameFromConfigKeys(conf,
-        OZONE_OM_ADDRESS_KEY);
-    this.host = omHost.orElse("0.0.0.0");
     this.omFailoverProxyProvider = new OMFailoverProxyProvider(conf, ugi,
         omServiceId);
+    // keeping mapping of hosts to channels (raw)
+    // similarly, hosts to clients (connections)
+    // connections Channels kept open throughout
+    // the sevice time of the s3g; internally
+    // io.grpc recovers from connection failures though
+    // connection retries with backoff strategy
+    this.channels = new HashMap<>();
+    this.clients = new HashMap<>();
+    this.conf = conf;
+    start();
+  }
 
-    NettyChannelBuilder channelBuilder =
-        NettyChannelBuilder.forAddress(this.host, port)
-            .usePlaintext()
-            .maxInboundMessageSize(OzoneConsts.OZONE_SCM_CHUNK_MAX_SIZE);
+  /* initializes hosts from configuration and returns single */
+  /* single host from set */
+  public String initHosts() {
+    String localOMServiceId = null;
+    localOMServiceId = conf.getTrimmed(OZONE_OM_INTERNAL_SERVICE_ID);
+    omServiceIds = localOMServiceId == null ?
+        conf.getTrimmedStringCollection(
+            OZONE_OM_SERVICE_IDS_KEY) :
+        Collections.singletonList(localOMServiceId);
 
-    channel = channelBuilder.build();
-    client = OzoneManagerServiceGrpc.newBlockingStub(channel);
+    List<String> oms = new ArrayList<>();
+    for (String serviceId : omServiceIds) {
+      Collection<String> omNodeIds = OmUtils.getOMNodeIds(conf, serviceId);
+      for (String nodeId : omNodeIds) {
+        Optional<String> hostaddr = getHostNameFromConfigKeys(conf,
+            ConfUtils.addKeySuffixes(OZONE_OM_ADDRESS_KEY,
+                serviceId, nodeId));
+        if (hostaddr.isPresent()) {
+          oms.add(hostaddr.get());
+        }
+      }
+    }
+
+    omAddresses = oms;
+    String hostaddr;
+    if (oms.size() == 0) {
+      Optional<String> omHost = getHostNameFromConfigKeys(conf,
+          OZONE_OM_ADDRESS_KEY);
+      hostaddr = omHost.orElse("0.0.0.0");
+      omAddresses.add(hostaddr);
+    } else {
+      hostaddr = omAddresses.get(0);
+    }
+    return hostaddr;
+  }
+
+  public void start() {
+    if (!isRunning.compareAndSet(false, true)) {
+      LOG.info("Ignore. already started.");
+      return;
+    }
+
+    host = initHosts();
+    port = conf.getObject(GrpcOmTransportConfig.class).getPort();
+
+    for (String host : omAddresses) {
+      NettyChannelBuilder channelBuilder =
+          NettyChannelBuilder.forAddress(host, port)
+              .usePlaintext()
+              .maxInboundMessageSize(OzoneConsts.OZONE_SCM_CHUNK_MAX_SIZE);
+
+      channels.put(host, channelBuilder.build());
+      clients.put(host,
+                  OzoneManagerServiceGrpc.newBlockingStub(channels.get(host)));
+    }
+
+    LOG.info("{}: started", CLIENT_NAME);
   }
 
   @Override
@@ -87,10 +167,10 @@ public class GrpcOmTransport implements OmTransport {
   public OMResponse submitRequest(OMRequest payload) throws IOException {
     UserGroupInformation ugi = UserGroupInformation.getCurrentUser();
 
-    for (TokenIdentifier tid: ugi.
-            getTokenIdentifiers()) {
+    for (TokenIdentifier tid : ugi.
+        getTokenIdentifiers()) {
       if (tid instanceof OzoneTokenIdentifier) {
-        OzoneTokenIdentifier oti = (OzoneTokenIdentifier)tid;
+        OzoneTokenIdentifier oti = (OzoneTokenIdentifier) tid;
         LOG.info(oti.toString());
         if (oti.getTokenType().equals(S3AUTHINFO)) {
           payload = OMRequest.newBuilder(payload)
@@ -105,7 +185,76 @@ public class GrpcOmTransport implements OmTransport {
       }
     }
     LOG.debug("OMRequest {}", payload);
-    return client.submitRequest(payload);
+    OMResponse resp = null;
+    boolean tryOtherHost = true;
+    while (tryOtherHost) {
+      tryOtherHost = false;
+      try {
+        resp = clients.get(host).submitRequest(payload);
+      } catch (io.grpc.StatusRuntimeException e) {
+        ResultCodes resultCode = ResultCodes.INTERNAL_ERROR;
+        if (e.getStatus().getCode() == Status.Code.UNAVAILABLE) {
+          resultCode = ResultCodes.TIMEOUT;
+        }
+        throw new OMException(e.getCause(), resultCode);
+      }
+      if (resp.getStatus() != OK) {
+        // check if om leader,
+        // if not tryOtherHost
+        tryOtherHost = isNotOmLeader(resp);
+      }
+    }
+    return resp;
+  }
+
+  @ConfigGroup(prefix = "ozone.om.protocolPB")
+  public static final class GrpcOmTransportConfig {
+    @Config(key = "port", defaultValue = "8981",
+        description = "Port used for"
+            + " the GrpcOmTransport OzoneManagerServiceGrpc server",
+        tags = {ConfigTag.MANAGEMENT})
+    private int port;
+
+    public int getPort() {
+      return port;
+    }
+
+    public GrpcOmTransportConfig setPort(int portParam) {
+      this.port = portParam;
+      return this;
+    }
+  }
+
+  private boolean isNotOmLeader(OMResponse resp) {
+    boolean isNotLeader = false;
+    try {
+      Class<?> realClass = Class.forName(resp.getMessage()
+                                         .substring(0, resp.getMessage()
+                                                    .indexOf(":")));
+      Class<? extends IOException> cls = realClass
+          .asSubclass(IOException.class);
+      Constructor<? extends IOException> cn = cls.getConstructor(String.class);
+      cn.setAccessible(true);
+      IOException ex = cn.newInstance(resp.getMessage());
+      LOG.info(ex.toString());
+      if (ex instanceof OMNotLeaderException) {
+        lastVisited = lastVisited == -1 ? hostIndex : lastVisited;
+        hostIndex = (hostIndex + 1) % omAddresses.size();
+        host = omAddresses.get(hostIndex);
+        if (hostIndex != lastVisited) {
+          isNotLeader = true;
+        } else {
+          lastVisited = -1;
+        }
+      }
+    } catch (java.lang.NoSuchMethodException |
+      java.lang.InstantiationException |
+      java.lang.IllegalAccessException |
+      java.lang.reflect.InvocationTargetException |
+      java.lang.StringIndexOutOfBoundsException |
+      java.lang.ClassNotFoundException e) {}
+
+    return isNotLeader;
   }
 
   @Override
@@ -131,17 +280,21 @@ public class GrpcOmTransport implements OmTransport {
   }
 
   public void shutdown() {
-    if (channel == null) {
+    if (channels == null) {
       return;
     }
-    channel.shutdown();
-    try {
-      channel.awaitTermination(5, TimeUnit.SECONDS);
-    } catch (Exception e) {
-      LOG.error("failed to shutdown OzoneManagerServiceGrpc channel", e);
-    } finally {
-      channel.shutdownNow();
-    }
+    channels.keySet().forEach(k -> {
+      ManagedChannel channel = channels.get(k);
+      channel.shutdown();
+      try {
+        channel.awaitTermination(5, TimeUnit.SECONDS);
+      } catch (Exception e) {
+        LOG.error("failed to shutdown OzoneManagerServiceGrpc channel", e);
+      } finally {
+        channel.shutdownNow();
+      }
+    });
+    isRunning.set(false);
   }
 
   @Override
@@ -150,3 +303,5 @@ public class GrpcOmTransport implements OmTransport {
     shutdown();
   }
 }
+
+
